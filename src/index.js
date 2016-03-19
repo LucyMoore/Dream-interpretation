3852914067ea7cdf7e90de60698070614056725b
LucyMoore <lucyviolet.moore@gmail.com>
2016-03-19 22:57:59 +1300

    requests are functioning together, working on appending result to page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
 var $ = require('jquery')
+var request = require('superagent')
 
 $(document).ready(function(){
 
   $('#elem1').blur(function(){
-    $('h1').html('hello')
+    var searchQuery = $('#elem1').val();
+    console.log(searchQuery)
+    getAnalysis(searchQuery, '#elem1')
+
+
+    
   });
 
+// $('#elem1').blur(function(){
+//     var searchQuery = $('#elem1').val();
+//     console.log(searchQuery)
+//   });
+
+
+// $('#elem1').blur(function(){
+//     var searchQuery = $('#elem1').val();
+//     console.log(searchQuery)
+//   });
  
+  function getAnalysis(search, returnTo){
+    request
+   .get('/api/v1/dreams')
+   .end(function(err, res){
+    var value = JSON.parse(res.text)
+    console.log(returnTo)
+    //return value.dreams.Guilt
+  $(returnTo).append('meow')
 
 
+   });
+  }
 
 
 })
