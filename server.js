0e4e80b2ccb0f90fee977e7953017670635cdafe
LucyMoore <lucyviolet.moore@gmail.com>
2016-03-19 21:59:58 +1300

    reading json file ok

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express();
+var fs = require('fs')
 
 // set the port to run on
 app.set('port', 3000);
@@ -12,3 +13,10 @@ var server = app.listen(app.get('port'), function() {
 var port = server.address().port;
   console.log('running on ' + port);
 });
+
+app.get('/api/v1/dreams', function(req,res){
+  fs.readFile('DB.json','utf8', function(err, data){
+    console.log(data)
+    res.json(JSON.parse(data))
+  })
+})
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 var $ = require('jquery')
 
 $(document).ready(function(){
+
+  $('#elem1').blur(function(){
+    $('h1').html('hello')
+  });
+
  
+
+
 
 
 })
