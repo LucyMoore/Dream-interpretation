72ae498ebcacfd40542316f3e8a09edc8c9276a5
LucyMoore <lucyviolet.moore@gmail.com>
2016-03-30 11:09:18 +1300

    branch to add new page

--- DB/DB.js
+++ DB/DB.js
@@ -2,16 +2,13 @@ module.exports = function (knex) {
   
   return {
     getDbAnalysis: function (table, query, callback) {
-    	var test = Object.keys(query)
-      	knex.raw('select * from ' + table + ' where element="' + query[test] + '"')
+    	var searchParams = Object.keys(query)
+      	knex.raw('select * from ' + table + ' where element="' + query[searchParams] + '"')
       	.then(function (resp) {
         	callback(null, resp)
       })
     },
 
-    
-
-    
   }
 }
 //[ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
